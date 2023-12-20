c2d28dcdd03df4dedae389621bd87c7baa77dfd6
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-20 20:39:05 +0545

    state completed with no img

--- statehooks/src/App.jsx
+++ statehooks/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{useState} from 'react'
+import itemlist from './constants/constant'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+export default function App(){
+  const [index, setIndex]=useState(0);
+  function handleEvent(){
+    if(index <2){
+      setIndex(index+1);
+    }
+    else{
+      setIndex(0);
+    }
+  }
+let item=itemlist[index]
+  return(
+    <div>
+      <button onClick={handleEvent}>
+        mybutton
+      </button>
+      <h1>{item.name}</h1>
+      <img src={item.img} alt="" />
+    </div>
   )
 }
-
-export default App
---
+++ statehooks/src/constants/constant.js
@@ -0,0 +1,15 @@
+const item=[
+    {
+        name: 'Ujjwal Puri',
+        img:'/home/uzi/Documents/Statehooks/statehooks/src/assets/img1.jpg'
+    },
+    {
+        name:'Alex Morgan',
+        img:'../assets/img2.jpg'
+    },
+    {
+        name:'James Roam',
+        img:'../assets/img3.jpg'
+    }
+]
+export default item;
