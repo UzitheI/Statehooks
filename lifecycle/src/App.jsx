08edfaf77ac71d21776a143cf0aad6e0cd0cd240
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-28 18:22:49 +0545

    added testing libraries

---
+++ lifecycle/src/App.jsx
@@ -0,0 +1,8 @@
+//when we use class components we cannot use useEffect to handle the components throughout its lifecycle, so we used different functions entirely
+
+/*
+render() is the most used
+
+//in the docs, class components are only really important when dealing with legacy documents
+
+*/
