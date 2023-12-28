96eba7b781b91b1578d8ab69277df90755152783
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-28 17:45:19 +0545

    created a class component

--- class/vite-project/src/components/ClassCom.jsx
+++ class/vite-project/src/components/ClassCom.jsx
@@ -14,18 +14,40 @@ class ClassCom extends Component{
             todos:[],
             inputVal:"",
         }
+        this.handleInput=this.handleInput.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+
+    }
+    handleInput(e){
+        this.setState((state)=>({
+            ...state,
+            inputVal:e.target.value,
+        }));
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        this.setState((state)=>({
+            todos:state.todos.concat(state.inputVal),
+            inputVal:"",
+        }))
     }
         render(){
             return(
                 <section>
                     <h3>{this.props.name}</h3>
-                    <form action="">
+                    <form action="" onSubmit={this.handleSubmit}>
                         <label htmlFor="task-entry">Enter a task:</label>
-                        <input type="text" name="task-entry" />
+                        <input type="text" name="task-entry" 
+                        value={this.state.inputVal}
+                        onChange={this.handleInput}/>
                         <button type="submit">Submit</button>
                     </form>
                     <h4>All the tasks</h4>
-                    <ul></ul>
+                    <ul>
+                        {this.state.todos.map((todo)=>(
+                            <li key={todo}>{todo}</li>
+                        ))}
+                    </ul>
 
                 </section>
             );
