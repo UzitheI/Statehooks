d30ca970d2061d903c2006cb4a7ee6fcdd37145c
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-31 20:39:29 +0545

    nesting

--- routers/src/components/Profile.jsx
+++ routers/src/components/Profile.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import {Outlet } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
-export default function Profile(){
-    return(
-        <div>
-            <p>This is the Profile SEction</p>
-            <h2>The profile is visite d and:</h2>
-            <Outlet/>
-        </div>
-    )
-}
+const Profile = () => {
+  const { name } = useParams();
+
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
+    </div>
+  );
+};
+
+export default Profile;
